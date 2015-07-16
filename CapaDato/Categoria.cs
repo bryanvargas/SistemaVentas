b3899545a6a979648034183a1d8cdc762f6b9979
Shingo Yabuki <bryanprsvar@gmail.com>
2015-07-16 01:16:53 -0300

    Se refactorizo el proyecto, avances significativos

--- CapaDato/Categoria.cs
+++ CapaDato/Categoria.cs
@@ -46,38 +46,39 @@ namespace CapaDato
         {
             string salida = "";
             SqlConnection conn = new SqlConnection();
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "psinsertar_categoria";
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            SqlParameter idcategoria = new SqlParameter();
-            idcategoria.ParameterName = "@idcategoria";
-            idcategoria.SqlDbType= SqlDbType.Int;
-            idcategoria.Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(id_categoria);
-
-            SqlParameter nombre = new SqlParameter();
-            nombre.ParameterName = "@nombre";
-            nombre.SqlDbType = SqlDbType.VarChar;
-            nombre.Size = 50;
-            nombre.Value = categoria.Nombre;
-            cmd.Parameters.Add(nombre);
-
-            SqlParameter descripcion = new SqlParameter();
-            descripcion.ParameterName = "@descripcion";
-            descripcion.SqlDbType = SqlDbType.VarChar;
-            descripcion.Size = 256;
-            descripcion.Value = categoria.Descripcion;
-            cmd.Parameters.Add(descripcion);
-
-
-            //ejecutamos nuestro comando
-            salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se ingreso el registro";
+          
             try
             {
                 conn.ConnectionString = Conexion.string_conn;
                 conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "psinsertar_categoria";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter idcategoria = new SqlParameter();
+                idcategoria.ParameterName = "@idcategoria";
+                idcategoria.SqlDbType = SqlDbType.Int;
+                idcategoria.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(idcategoria);
+
+                SqlParameter nombre = new SqlParameter();
+                nombre.ParameterName = "@nombre";
+                nombre.SqlDbType = SqlDbType.VarChar;
+                nombre.Size = 50;
+                nombre.Value = categoria.Nombre;
+                cmd.Parameters.Add(nombre);
+
+                SqlParameter descripcion = new SqlParameter();
+                descripcion.ParameterName = "@descripcion";
+                descripcion.SqlDbType = SqlDbType.VarChar;
+                descripcion.Size = 256;
+                descripcion.Value = categoria.Descripcion;
+                cmd.Parameters.Add(descripcion);
+
+
+                //ejecutamos nuestro comando
+                salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se ingreso el registro";
             }
             catch (Exception ex)
             {
@@ -98,38 +99,39 @@ namespace CapaDato
         {
             string salida = "";
             SqlConnection conn = new SqlConnection();
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "speditar_categoria";
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            SqlParameter idcategoria = new SqlParameter();
-            idcategoria.ParameterName = "@idcategoria";
-            idcategoria.SqlDbType = SqlDbType.Int;
-            idcategoria.Value = categoria.ID_categoria;
-            cmd.Parameters.Add(id_categoria);
-
-            SqlParameter nombre = new SqlParameter();
-            nombre.ParameterName = "@nombre";
-            nombre.SqlDbType = SqlDbType.VarChar;
-            nombre.Size = 50;
-            nombre.Value = categoria.Nombre;
-            cmd.Parameters.Add(nombre);
-
-            SqlParameter descripcion = new SqlParameter();
-            descripcion.ParameterName = "@descripcion";
-            descripcion.SqlDbType = SqlDbType.VarChar;
-            descripcion.Size = 256;
-            descripcion.Value = categoria.Descripcion;
-            cmd.Parameters.Add(descripcion);
-
-
-            //ejecutamos nuestro comando
-            salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se actualizo el registro";
+            
             try
             {
                 conn.ConnectionString = Conexion.string_conn;
                 conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "speditar_categoria";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter idcategoria = new SqlParameter();
+                idcategoria.ParameterName = "@idcategoria";
+                idcategoria.SqlDbType = SqlDbType.Int;
+                idcategoria.Value = categoria.ID_categoria;
+                cmd.Parameters.Add(idcategoria);
+
+                SqlParameter nombre = new SqlParameter();
+                nombre.ParameterName = "@nombre";
+                nombre.SqlDbType = SqlDbType.VarChar;
+                nombre.Size = 50;
+                nombre.Value = categoria.Nombre;
+                cmd.Parameters.Add(nombre);
+
+                SqlParameter descripcion = new SqlParameter();
+                descripcion.ParameterName = "@descripcion";
+                descripcion.SqlDbType = SqlDbType.VarChar;
+                descripcion.Size = 256;
+                descripcion.Value = categoria.Descripcion;
+                cmd.Parameters.Add(descripcion);
+
+
+                //ejecutamos nuestro comando
+                salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se actualizo el registro";
             }
             catch (Exception ex)
             {
@@ -149,23 +151,24 @@ namespace CapaDato
         {
             string salida = "";
             SqlConnection conn = new SqlConnection();
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "speliminar_categoria";
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            SqlParameter idcategoria = new SqlParameter();
-            idcategoria.ParameterName = "@idcategoria";
-            idcategoria.SqlDbType = SqlDbType.Int;
-            idcategoria.SqlDbType = SqlDbType.Int;
-            cmd.Parameters.Add(id_categoria);
-
-            //ejecutamos nuestro comando
-            salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se elimino el registro";
+          
             try
             {
                 conn.ConnectionString = Conexion.string_conn;
                 conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "speliminar_categoria";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter idcategoria = new SqlParameter();
+                idcategoria.ParameterName = "@idcategoria";
+                idcategoria.SqlDbType = SqlDbType.Int;
+                idcategoria.Value = categoria.ID_categoria;
+                cmd.Parameters.Add(idcategoria);
+
+                //ejecutamos nuestro comando
+                salida = cmd.ExecuteNonQuery() == 1 ? "OK" : "no se elimino el registro";
             }
             catch (Exception ex)
             {
