14d300e766f0bb5c4badc5c4af1656b24fd6da53
Shingo Yabuki <bryanprsvar@gmail.com>
2015-07-17 20:50:03 -0300

    Implementacion de NPresentacion de capa Negocio

--- CapaNegocio/NPresentacion.cs
+++ CapaNegocio/NPresentacion.cs
@@ -10,40 +10,40 @@ namespace CapaNegocio
 {
     public class NPresentacion
     {
-        //metodo Insertar que llama al metodo Insertar de la clase Categoria
+        //metodo Insertar que llama al metodo Insertar de la clase Presentacion
         //de la capa datos
         public static string Insertar(string nombre, string descripcion)
         {
-            Categoria Obj_datos = new Categoria();
+            DPresentacion Obj_datos = new DPresentacion();
             Obj_datos.Nombre = nombre;
             Obj_datos.Descripcion = descripcion;
             return Obj_datos.Insertar(Obj_datos);
         }
 
-        public static string Editar(int id_categoria, string nombre, string descripcion)
+        public static string Editar(int id_presentacion, string nombre, string descripcion)
         {
-            Categoria Obj_datos = new Categoria();
-            Obj_datos.ID_categoria = id_categoria;
+            DPresentacion Obj_datos = new DPresentacion();
+            Obj_datos.IdPresentacion = id_presentacion;
             Obj_datos.Nombre = nombre;
             Obj_datos.Descripcion = descripcion;
             return Obj_datos.Editar(Obj_datos);
         }
 
-        public static string Eliminar(int id_categoria)
+        public static string Eliminar(int id_presentacion)
         {
-            Categoria Obj_datos = new Categoria();
-            Obj_datos.ID_categoria = id_categoria;
+            DPresentacion Obj_datos = new DPresentacion();
+            Obj_datos.IdPresentacion = id_presentacion;
             return Obj_datos.Eliminar(Obj_datos);
         }
 
         public static DataTable Mostrar()
         {
-            return new Categoria().Mostrar();
+            return new DPresentacion().Mostrar();
         }
 
         public static DataTable BuscarNombre(string texto_buscar)
         {
-            Categoria Obj_data = new Categoria();
+            DPresentacion Obj_data = new DPresentacion();
             Obj_data.TextoBuscar = texto_buscar;
             return Obj_data.BuscarNombre(Obj_data);
         }
